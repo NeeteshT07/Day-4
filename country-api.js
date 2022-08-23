86306846bad50e77158ff059cfb5c4c8d55fae4c
NeeteshT07 <111701955+NeeteshT07@users.noreply.github.com>
2022-08-23 23:53:22 +0530

    Add files via upload

---
+++ country-api.js
@@ -0,0 +1,26 @@
+// XMLHttpRequest
+console.log("JS File Executed");
+
+// Step 1
+// Create Obj which is capable of making an API Call
+const request = new XMLHttpRequest();
+
+// Step 2
+// Open connection to destination or config details to Http request obj
+request.open('GET', 'https://restcountries.com/v3/all'); // Setting the Operation to be Read Operation
+
+// Step 3
+// Send the request to rest countries server
+request.send(null);
+
+// Step 3.1
+// getting the response and unpacking
+request.onload = function () {
+  const response = JSON.parse(request.responseText); // parse the string of JSON
+  console.log(response);
+
+response.forEach(({ flag, name: { common } }) => { // destructured flag from each country
+  console.log(flag, common);
+});
+
+};
